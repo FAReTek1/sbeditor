9498655bffa1659f94d861f678fe65a33153c2b1
FA ReTek <ldashunderscoreo@gmail.com>
2024-10-30 21:59:24 +0000

    automated setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 from setuptools import setup
+import subprocess
+
+# Copied from https://www.youtube.com/watch?v=U-aIPTS580s
+remote_version = subprocess.run(['git', 'describe', '--tags'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+assert '.' in remote_version
+
+with open('README.md', 'r', encoding="utf-8") as readme:
+    long_description = readme.read()
+
+with open("remote version.txt", "w") as remote_version_file:
+    remote_version_file.write(remote_version)
+print(remote_version)
 
 setup(
     name='sbeditor',
-    version='0.0.1',
+    version=remote_version,
     packages=['sbeditor'],
     url='https://github.com/FAReTek1/sbeditor',
     license='MIT',
     author='faretek1',
     author_email='',
     description='A parser for all things sb3',
-    long_description=
-    """
-    A parser for all things sb3! This is a module made by faretek1 on scratch
-    """
+    long_description=long_description
 )
