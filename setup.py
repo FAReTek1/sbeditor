966a20a3eb08f4be44d406a724f57957fe0aef0f
FA ReTek <ldashunderscoreo@gmail.com>
2024-10-30 14:37:57 +0000

    created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sbeditor',
+    version='0.0.1',
+    packages=['sbeditor'],
+    url='https://github.com/FAReTek1/sbeditor',
+    license='MIT',
+    author='faretek1',
+    author_email='',
+    description='A parser for all things sb3'
+)
