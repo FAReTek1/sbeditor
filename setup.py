cbc8a0545c71657497192e01651730aa874a5031
FA ReTek <ldashunderscoreo@gmail.com>
2024-10-31 21:13:40 +0000

    new version!

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.md', 'r', encoding="utf-8") as readme:
 
 setup(
     name='sbeditor',
-    version="v0.0.2",
+    version="v0.0.3",
     packages=['sbeditor'],
     url='https://github.com/FAReTek1/sbeditor',
     license='MIT',
