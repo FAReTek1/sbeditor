6291634620ff5c176e237500312895844d63be37
FA ReTek <ldashunderscoreo@gmail.com>
2024-11-09 10:17:14 +0000

    more concise ultrajoin

--- sbeditor/plugins/strutil/actions.py
+++ sbeditor/plugins/strutil/actions.py
@@ -1,11 +1,10 @@
 from ...sbuild import *
 
+
 def UltraJoin(*args: list[Block | Input]):
     if len(args) == 1:
         return args[0]
 
-    join = Operators.Join()
-    join.set_string1(args[0])
-    join.set_string2(UltraJoin(*args[1:]))
-
-    return join
+    return (Operators.Join()
+            .set_string1(args[0])
+            .set_string2(UltraJoin(*args[1:])))
